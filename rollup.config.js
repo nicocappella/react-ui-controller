7ddfbf0dc96aa0a45b12fa2e948cee08e220ec11
Nicolas Cappella <nicolascappellabb@gmail.com>
2022-10-06 16:04:01 -0300

    Second commit

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import { terser } from 'rollup-plugin-terser';
 
 const packageJson = require('./package.json');
 
@@ -20,8 +21,18 @@ export default [
                 sourcemap: true,
             },
         ],
-        plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })],
-        external: ['react', 'react-dom'],
+        plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), terser()],
+        external: [
+            'react',
+            'react-dom',
+            '@emotion/react',
+            '@emotion/styled',
+            '@mui/lab',
+            '@mui/icons-material',
+            '@mui/material',
+            '@mui/material/styles',
+            '@mui/styles',
+        ],
     },
     {
         input: 'dist/esm/types/index.d.ts',
