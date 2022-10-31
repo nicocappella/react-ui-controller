df04871dc47b6a973f04b556d39434f0371f3e31
Nicolas Cappella <nicolascappellabb@gmail.com>
2022-10-31 12:46:50 -0300

    Href on LinkButton

--- src/view/ui/Atoms/Inputs/Buttons/LinkButton/LinkButton.tsx
+++ src/view/ui/Atoms/Inputs/Buttons/LinkButton/LinkButton.tsx
@@ -5,11 +5,12 @@ export interface ILinkButton {
     text: string;
     color: string;
     underline?: 'always' | 'hover' | 'none';
+    href: string;
 }
 
 const LinkButton = (props: ILinkButton) => {
     return (
-        <Link color={props.color} underline={props.underline ? props.underline : 'none'} component="a" variant="subtitle1">
+        <Link color={props.color} underline={props.underline ? props.underline : 'none'} component="a" variant="subtitle1" href={props.href}>
             {props.text}
         </Link>
     );
