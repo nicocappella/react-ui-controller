2ea77bbca6ea625fd8dc7ef9c3e93680a62a5824
Nicolas Cappella <nicolascappellabb@gmail.com>
2022-10-31 12:08:58 -0300

    Export LinkButton

--- src/view/ui/Atoms/Inputs/Buttons/index.ts
+++ src/view/ui/Atoms/Inputs/Buttons/index.ts
@@ -1,3 +1,4 @@
 export { Button } from './Button/Button';
 export { FAB } from './FAB/FAB';
 export { IconButton } from './IconButton/IconButton';
+export { LinkButton } from './LinkButton/LinkButton';
