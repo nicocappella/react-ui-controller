ae4e2843c05d657285884eacd95e9ed03ea47acb
Nicolas Cappella <nicolascappellabb@gmail.com>
2022-11-03 16:04:00 -0300

    Fix carrousel

--- src/view/ui/Organism/Surfaces/Carrousel/Carrousel.stories.tsx
+++ src/view/ui/Organism/Surfaces/Carrousel/Carrousel.stories.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-
 import { Carrousel } from './Carrousel';
+import { Home } from '@mui/icons-material';
+// import { Button, LinkButton } from '../../../Atoms';
 
 export default {
-  title: 'React-Ui-Controller/Organism/Surfaces/Carrousel',
-  component: Carrousel,
+    title: 'React-Ui-Controller/Organism/Surfaces/Carrousel',
+    component: Carrousel,
 } as ComponentMeta<typeof Carrousel>;
 
 const Template: ComponentStory<typeof Carrousel> = (args) => <Carrousel {...args} />;
 
 export const NormalCarrousel = Template.bind({});
-NormalCarrousel.args = {}
+NormalCarrousel.args = {
+    cards: [{ img: '', text: 'Hola soy el 1', title: 'Hola soy el 1' }, { img: '', text: 'Hola soy el 2', title: 'Hola soy el 2' }],
+};
+
+export const SmallCarrousel = Template.bind({});
+SmallCarrousel.args = {};
+
+export const LargeCarrousel = Template.bind({});
+LargeCarrousel.args = {};
--- src/view/ui/Organism/Surfaces/Carrousel/Carrousel.tsx
+++ src/view/ui/Organism/Surfaces/Carrousel/Carrousel.tsx
@@ -1,65 +1,66 @@
-import { PropaneSharp } from "@mui/icons-material";
-import { Box, Typography } from "@mui/material";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import { PropaneSharp } from '@mui/icons-material';
+import { Box, Typography } from '@mui/material';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
-interface IProps {
-  cards: { title: string; text: string; img: string }[];
+export interface IProps {
+    cards: { title: string; text: string; img: React.ReactNode }[];
 }
 
 export const Carrousel = (props: IProps) => {
-  const [current, setCurrent] = useState(0);
-  const [direction, setDirection] = useState<"left" | "right">("right");
+    const [current, setCurrent] = useState(0);
+    const [direction, setDirection] = useState<'left' | 'right'>('right');
 
-  const nextSlide = () => {
-    setCurrent(current === props.cards.length - 1 ? 0 : current + 1);
-    setDirection("left");
-  };
+    const nextSlide = () => {
+        setCurrent(current === props.cards.length - 1 ? 0 : current + 1);
+        setDirection('left');
+    };
 
-  const prevSlide = () => {
-    setCurrent(current === 0 ? props.cards.length - 1 : current - 1);
-  };
+    const prevSlide = () => {
+        setCurrent(current === 0 ? props.cards.length - 1 : current - 1);
+    };
 
-  useEffect(() => {}, [current]);
-  return (
-    <div>
-      <Box
-        sx={{
-          position: "absolute",
-          left: 10,
-          zIndex: 10,
-          top: "50%",
-          cursor: "pointer",
-        }}
-        onClick={prevSlide}
-      >
-        <ArrowBackIosIcon />
-      </Box>
-      <Box
-        sx={{
-          position: "absolute",
-          right: 10,
-          zIndex: 10,
-          top: "50%",
-          cursor: "pointer",
-        }}
-        onClick={nextSlide}
-      >
-        <ArrowForwardIosIcon />
-      </Box>
-      {props.cards.map((d, i) => (
+    useEffect(() => {}, [current]);
+    return (
         <Box>
-          <Image src={d.img} alt="image" />
-          <Box>
-            <Typography variant="h5" fontWeight="bold">
-              {d.title && d.title.toUpperCase()}
-            </Typography>
-            <Typography variant="subtitle1">{d.text}</Typography>
-          </Box>
+            <Box>
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        left: 10,
+                        zIndex: 10,
+                        top: '50%',
+                        cursor: 'pointer',
+                    }}
+                    onClick={prevSlide}
+                >
+                    <ArrowBackIosIcon />
+                </Box>
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        right: 10,
+                        zIndex: 10,
+                        top: '50%',
+                        cursor: 'pointer',
+                    }}
+                    onClick={nextSlide}
+                >
+                    <ArrowForwardIosIcon />
+                </Box>
+            </Box>
+            <Box display="flex" sx={{ transform: `translate(${-current * 100}%)` }}>
+                {props.cards.map((d, i) => (
+                    <Box key={i} sx={{ minWidth: '100vw' }}>
+                        <Typography variant="h5" fontWeight="bold">
+                            {d.title && d.title.toUpperCase()}
+                        </Typography>
+                        <Typography variant="subtitle1">{d.text}</Typography>
+                    </Box>
+                ))}
+            </Box>
         </Box>
-      ))}
-    </div>
-  );
+    );
 };
