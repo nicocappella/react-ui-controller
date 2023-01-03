34f887fdfa19b22ad5852678cfdf790e2554cc34
Nicolas Cappella <nicolascappellabb@gmail.com>
2023-01-03 20:48:51 -0300

    Changes on TopAppBar

--- src/view/ui/Organism/Surfaces/TopAppBar/TopAppBar.tsx
+++ src/view/ui/Organism/Surfaces/TopAppBar/TopAppBar.tsx
@@ -4,32 +4,47 @@ import { Box } from '@mui/system';
 export interface IProps {
     logo?: { component: React.ReactNode };
     navButtons?: { component: React.ReactNode }[];
+    alignNavButtons?: 'center' | 'flex-start' | 'flex-end' | 'space-around' | 'space-between' | 'space-evenly';
+    alignOtherButtons?: 'center' | 'flex-start' | 'flex-end' | 'space-around' | 'space-between' | 'space-evenly';
     otherButtons?: { component: React.ReactNode }[];
     showMenu?: boolean;
     background?: string;
     boxShadow?: string;
     position?: 'absolute' | 'fixed' | 'relative' | 'static' | 'sticky';
     horizontalPadding?: string | number;
+    height?: string | number;
 }
 
-const TopAppBar = (props: IProps) => {
+const TopAppBar = ({
+    logo,
+    navButtons,
+    alignNavButtons = 'center',
+    otherButtons,
+    alignOtherButtons = 'center',
+    showMenu,
+    background = 'transparent',
+    boxShadow,
+    position = 'fixed',
+    horizontalPadding,
+    height = 64,
+}: IProps) => {
     return (
         <AppBar
-            position={props.position ? props.position : 'fixed'}
+            position={position}
             sx={{
-                background: props.background,
-                boxShadow: props.boxShadow,
-                paddingLeft: props.horizontalPadding,
-                paddingRight: props.horizontalPadding,
+                background: background,
+                boxShadow: boxShadow,
+                paddingLeft: horizontalPadding,
+                paddingRight: horizontalPadding,
             }}
         >
-            <Box component="nav" display="flex" justifyContent="space-between" sx={{ maxHeight: 64 }} alignItems="center">
-                <Box component="div">{props.logo?.component}</Box>
-                <Box display="flex" justifyContent="space-between" alignItems="center" gap={5}>
-                    {props.navButtons && props.navButtons.map((d, i) => <div key={`navButton-${i}`}>{d.component}</div>)}
+            <Box component="nav" display="flex" justifyContent="space-between" sx={{ height }} alignItems="center">
+                <Box component="div">{logo?.component}</Box>
+                <Box display="flex" justifyContent={alignNavButtons} alignItems="center" gap={5}>
+                    {navButtons && navButtons.map((d, i) => <Box key={`navButton-${i}`}>{d.component}</Box>)}
                 </Box>
-                <Box display="flex" justifyContent="space-between" alignItems="center">
-                    {props.otherButtons && props.otherButtons.map((d, i) => <div key={`otherButtons-${i}`}>{d.component}</div>)}
+                <Box display="flex" justifyContent={alignOtherButtons} alignItems="center">
+                    {otherButtons && otherButtons.map((d, i) => <Box key={`otherButtons-${i}`}>{d.component}</Box>)}
                 </Box>
             </Box>
             <Toolbar></Toolbar>
