155c4f507b0ba22571390396408fc24384b15cfa
Nicolas Cappella <nicolascappellabb@gmail.com>
2023-06-29 12:10:17 -0300

    Forget to export Loader

--- src/view/ui/Molecules/index.ts
+++ src/view/ui/Molecules/index.ts
@@ -2,5 +2,6 @@ export * from './Card';
 export * from './Dialogs';
 export * from './Forms';
 // export * from './Menu';
+export * from './Loader';
 export * from './Tabs';
 export * from './Breadcrumb';
